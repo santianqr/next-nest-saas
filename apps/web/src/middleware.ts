17fa9de1fba4181cd1851643801b6502d5d4b979
Santiago Andres Quintero Rodriguez <santianqr@gmail.com>
2025-03-01 22:54:16 -0500

    feat(auth): migrate middleware to new location and add session logging

--- apps/web/src/middleware.ts
+++ apps/web/src/middleware.ts
@@ -3,10 +3,11 @@ import { NextRequest, NextResponse } from "next/server";
 
 export default async function middleware(req: NextRequest) {
   const session = await getSession();
-
-  if (!session || !session.user)
+  console.log("session", session);
+  if (!session || !session.user) {
     return NextResponse.redirect(new URL("/auth/signin", req.nextUrl));
-
+  }
+  
   NextResponse.next();
 }
 
